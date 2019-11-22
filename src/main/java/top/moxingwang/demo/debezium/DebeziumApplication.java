414cfc90bd97636e46c74382a4a80ee5e246e9d3
moxingwang <xingwang.mo@chinaredstar.com>
2019-11-22 10:05:10 +0800

    init

---
+++ src/main/java/top/moxingwang/demo/debezium/DebeziumApplication.java
@@ -0,0 +1,13 @@
+package top.moxingwang.demo.debezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumApplication.class, args);
+	}
+
+}
